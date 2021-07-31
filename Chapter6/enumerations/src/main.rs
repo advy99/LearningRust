40042176042120fafcc035a8644ada9fe6e6d8f8
Antonio David Villegas Yeguas <advy99@gmail.com>
2021-07-31 22:16:47 +0200

    cambiando los tipos en el enum

--- Chapter6/enumerations/src/main.rs
+++ Chapter6/enumerations/src/main.rs
@@ -1,26 +1,12 @@
-enum IpAddrKind {
-	V4,
-	V6,
-}
-
-struct IpAddr {
-	kind: IpAddrKind,
-	address: String,
+enum IpAddr {
+	V4 (u8, u8, u8, u8),
+	V6 (String),
 }
 
 fn main() {
-	let ip_tipo_cuatro = IpAddrKind::V4;
-	let ip_tipo_seis = IpAddrKind::V6;
 
-	let home = IpAddr {
-		kind: IpAddrKind::V4,
-		address: String::from("127.0.0.1"),
-	};
+	let home = IpAddr::V4(127, 0, 0,1);
 
-	let loopback = IpAddr {
-		kind: IpAddrKind::V6,
-		address: String::from("::1"),
-	};
-
+	let loopback = IpAddr::V6(String::from("::1"));
 
 }
